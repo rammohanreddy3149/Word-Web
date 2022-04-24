d8115026a74c0a5f43bc8c88fcad2538330cafaa
Ram Mohan Reddy <rammohanreddy3149@gmail.com>
2022-04-24 17:58:04 +0530

    added Switching btw Dark and Light Mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Container } from '@material-ui/core';
+import { Container, Switch, withStyles } from '@material-ui/core';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Definition from './components/Definitions/Definition';
 import Header from './components/Header';
+import { grey } from "@material-ui/core/colors";
 
 function App() {
 
@@ -23,15 +24,50 @@ function App() {
     }
   };
 
-  console.log(meanings);
-
   useEffect(() => {
     dictionaryApi();
   }, [word, category]);
 
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
   return (
-    <div className="App" style={{ height: '100vh', backgroundColor: '#282c34', color: 'white' }}>
-      <Container maxWidth="md" style={{ display: 'flex', flexDirection: "column", height: '100vh' }}>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}>
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}>
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
         <Header setWord={setWord}
           category={category}
           setCategory={setCategory}
--- src/components/Definitions/Definition.js
+++ src/components/Definitions/Definition.js
@@ -4,13 +4,54 @@ import './Definition.css'
 const Definition = ({ meanings, word, LightTheme, category }) => {
     return (
         <div className='meanings'>
+            {meanings[0] && word && category === "en" && (
+                <audio
+                    style={{ 
+                        backgroundColor: "#fff",
+                        borderRadius: 10,
+                        width: "auto",
+                        minHeight: "40px" 
+                    }}
+                    src={
+                        meanings[0].phonetics[0] && meanings[0].phonetics[0].audio
+                    }
+                    controls preload="auto"
+                >
+                    Your browser does not support the audio element.
+                </audio>
+            )}
+
             {word === "" ? (
                 <span className='subTitle'>Give a search....!!!</span>
             ) : (
-                "Wow, You've typed", word
+                meanings.map((meaning) =>
+                    meaning.meanings.map((item) =>
+                        item.definitions.map((definition) => (
+                            <div
+                                className='singleMean'
+                                style={{
+                                    backgroundColor: LightTheme ? "#3b5360" : "white",
+                                    color: LightTheme ? "white" : "black",
+                                }}>
+                                <b>{definition.definition}</b>
+                                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                                {definition.example && (
+                                    <span>
+                                        <b>Example :</b> {definition.example}
+                                    </span>
+                                )}
+                                {definition.synonyms && (
+                                    <span>
+                                        <b>Synonyms :</b> {definition.synonyms.map((s) => `${s}, `)}
+                                    </span>
+                                )}
+                            </div>
+                        ))
+                    )
+                )
             )}
         </div>
-    )
-}
+    );
+};
 
-export default Definition
+export default Definition;
