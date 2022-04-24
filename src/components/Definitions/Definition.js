92962b72747df5bd615563ac02dde50299ce431b
Ram Mohan Reddy <rammohanreddy3149@gmail.com>
2022-04-24 17:17:13 +0530

    added Definition css

--- src/components/Definitions/Definition.js
+++ src/components/Definitions/Definition.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Definition.css'
 
 const Definition = ({ meanings, word, LightTheme, category }) => {
-  return (
-    <div>Definition</div>
-  )
+    return (
+        <div>Definition</div>
+    )
 }
 
 export default Definition
