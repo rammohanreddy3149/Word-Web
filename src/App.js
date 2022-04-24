b90ec5c1129465b559dc6e9391479f3ee63d97d0
Ram Mohan Reddy <rammohanreddy3149@gmail.com>
2022-04-24 15:38:49 +0530

    used dictionaryapi to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [word, setWord] = useState([]);
+  const [meanings, setMeanings] = useState([]);
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        'https://api.dictionaryapi.dev/api/v2/entries/en/plane'
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
-    <div className="App">Hello World</div>
+    <div className="App" style={{ height: '100vh', backgroundColor: '#282c34', color: 'white' }}>
+      <Container maxWidth="md" style={{ display: 'flex', flexDirection: "column", height:'100vh'}}>
+        Word Web
+      </Container>
+    </div>
   );
 }
 
